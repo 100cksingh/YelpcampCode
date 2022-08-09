4edba2dbdc7683d89f9f8bece71e4da2c2dfa993
CHANDRA KANT SINGH <91086975+100cksingh@users.noreply.github.com>
2022-08-09 18:49:58 +0530

    1st update
    first update

--- models/campground.js
+++ models/campground.js
@@ -1,13 +1,17 @@
+
 const mongoose =require('mongoose');
 const Schema=mongoose.Schema;
 
 const CampgroundSchema=new Schema({
     title:String,
-    price:String,
+    image:String,
+    price:Number,
     description:String,
     location:String
-})
+});
 
 module.exports=mongoose.model('Campground',CampgroundSchema);
+
+
 
 
