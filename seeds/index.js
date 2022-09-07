e2c84221b3a77ad1f3ca7e0aa41581a970ae4045
Chandra Kant Singh <91086975+100cksingh@users.noreply.github.com>
2022-09-07 14:28:11 +0530

    Add files via upload

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const seedDB=async ()=>{
         const camp=new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:'https://source.unsplash.com/collection/483251',
+            image:'https://source.unsplash.com/collection/483251/1600x900',
             description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit tempore accusantium commodi harum aspernatur, quibusdam praesentium deserunt doloremque optio error nisi eligendi iure dolorum ea exercitationem. Fugiat sequi esse sunt.'
             
         })
