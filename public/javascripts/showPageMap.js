e03e0d19e0651177aacda0b6768d5cc540aecf2c
Chandra Kant Singh <91086975+100cksingh@users.noreply.github.com>
2022-10-10 23:06:19 +0530

    CenterMap
    Centering the map update.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,7 +2,11 @@ mapboxgl.accessToken =mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 4 // starting zoom
+center: campground.geometry.coordinates, // starting position [lng, lat]
+zoom: 10 // starting zoom
 // projection: 'globe' // display the map as a 3D globe
 });
+
+new mapboxgl.Marker()
+   .setLngLat(campground.geometry.coordinates)
+   .addTo(map)
